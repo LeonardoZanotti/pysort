bc280c3baec5760831b37730600df250eda07bfd
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-06-13 12:26:35 -0300

    fixed functions

--- pysort.py
+++ pysort.py
@@ -23,21 +23,8 @@ def bogoSort(i):
 
 # Bubble Sort
 def bubbleSort(i):
-    """In-place bubble sort."""
-
-    if len(A) == 1:
-        return
-
-    swapped = True
-    for i in range(len(A) - 1):
-        if not swapped:
-            break
-        swapped = False
-        for j in range(len(A) - 1 - i):
-            if A[j] > A[j + 1]:
-                swap(A, j, j + 1)
-                swapped = True
-            yield A
+    print()
+    # sorting..
 
 # Bucket Sort
 def bucketSort(i):
@@ -51,52 +38,13 @@ def heapSort(i):
 
 # Insertion Sort
 def insertionSort(i):
-    """In-place insertion sort."""
-
-    for i in range(1, len(A)):
-        j = i
-        while j > 0 and A[j] < A[j - 1]:
-            swap(A, j, j - 1)
-            j -= 1
-            yield A
+    print()
+    # sorting..
 
 # Merge Sort
-def mergeSort(A, start, end):
-    """Merge sort."""
-    if (end <= start):
-        return
-    mid = start + ((end - start + 1) // 2) - 1
-    yield from mergesort(A, start, mid)
-    yield from mergesort(A, mid + 1, end)
-    yield from merge(A, start, mid, end)
-    yield A
-
-def merge(A, start, mid, end):
-    """Helper function for merge sort."""
-    
-    merged = []
-    leftIdx = start
-    rightIdx = mid + 1
-
-    while leftIdx <= mid and rightIdx <= end:
-        if A[leftIdx] < A[rightIdx]:
-            merged.append(A[leftIdx])
-            leftIdx += 1
-        else:
-            merged.append(A[rightIdx])
-            rightIdx += 1
-
-    while leftIdx <= mid:
-        merged.append(A[leftIdx])
-        leftIdx += 1
-
-    while rightIdx <= end:
-        merged.append(A[rightIdx])
-        rightIdx += 1
-
-    for i, sorted_val in enumerate(merged):
-        A[start + i] = sorted_val
-        yield A
+def mergeSort(i):
+    print()
+    # sorting..
 
 # Radix Sort
 def radixSort(i):
@@ -105,22 +53,8 @@ def radixSort(i):
 
 # Selection Sort
 def selectionSort(i):
-    """In-place selection sort."""
-    if len(A) == 1:
-        return
-
-    for i in range(len(A)):
-        # Find minimum unsorted value.
-        minVal = A[i]
-        minIdx = i
-        for j in range(i, len(A)):
-            if A[j] < minVal:
-                minVal = A[j]
-                minIdx = j
-            yield A
-        swap(A, i, minIdx)
-        yield A
-
+    print()
+    # sorting..
 
 # Smooth Sort
 def smoothSort(i):
@@ -129,44 +63,34 @@ def smoothSort(i):
 
 # Quick Sort
 def quickSort(A, start, end):
-    """In-place quicksort."""
-
     if start >= end:
         return
 
     pivot = A[end]
-    pivotIdx = start
+    tovip = start
 
     for i in range(start, end):
         if A[i] < pivot:
-            swap(A, i, pivotIdx)
-            pivotIdx += 1
+            swap(A, i, tovip)
+            tovip += 1
         yield A
-    swap(A, end, pivotIdx)
+    swap(A, end, tovip)
     yield A
 
-    yield from quickSort(A, start, pivotIdx - 1)
-    yield from quickSort(A, pivotIdx + 1, end)
+    yield from quickSort(A, start, tovip - 1)
+    yield from quickSort(A, tovip + 1, end)
 
 def swap(A, i, j):
-    """Helper function to swap elements i and j of list A."""
-
     if i != j:
         A[i], A[j] = A[j], A[i]
 
 operations = 0
-def animate(A, rects):
+def animate(values, rects):
     global operations
-    print(A, rects)
-    # for a, b in enumerate(barcollection):
-        # b.set_height(y[a])
-    for rect, val in zip(rects, A):
-        rect.set_height(val)
+    for rect, value in zip(rects, values):
+        rect.set_height(value)
     operations += 1
-    print(operations)
     text.set_text('Number of operations: {}'.format(operations))
-
-    # text.set_text("# of operations: {}".format(operations))
 
 def main():
     args = sys.argv
